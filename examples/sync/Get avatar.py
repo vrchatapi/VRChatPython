893d10651ff07908e7a55fb29db4b036bebfd6e4
Katistix <33645731+Katistic@users.noreply.github.com>
2020-05-07 07:56:59 +1000

    Get avatar examples

---
+++ examples/sync/Get avatar.py
@@ -0,0 +1,15 @@
+import vrcpy
+
+def main():
+    # Initialise vrcpy wrapper client and login with username + password
+    client = vrcpy.Client()
+    client.login("username", "password")
+
+    # Get avatar via id
+    a = client.fetch_avatar("avtr_fa5303c6-78d1-451c-a678-faf3eadb5c50")
+    author = a.author() # Get author of the avatar
+    print("Avatar '"+a.name+"' was made "+author.displayName)
+    ## This should print "Avatar 'Etoigne' was made Katfish"
+
+if __name__ == "__main__":
+    main()
