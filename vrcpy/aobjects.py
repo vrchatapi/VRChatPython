d071a3ce0b5274763d07af67ad14a730c3e2003e
Katistix <33645731+Katistic@users.noreply.github.com>
2020-05-06 19:04:34 +1000

    Add author() function in Avatar class
    Get avatar author's UserObject

--- vrcpy/aobjects.py
+++ vrcpy/aobjects.py
@@ -1,1 +1,7 @@
 from vrcpy.objects import *
+
+class Avatar(Avatar):
+    async def author(self):
+        resp = await self.client.api.call("/users/"+self.authorId)
+        self.client._raise_for_status(resp)
+        return User(self.client, resp["data"])
--- vrcpy/objects.py
+++ vrcpy/objects.py
@@ -42,6 +42,11 @@ class BaseObject:
 class Avatar(BaseObject):
     objType = "Avatar"
 
+    def author(self):
+        resp = self.client.api.call("/users/"+self.authorId)
+        self.client._raise_for_status(resp)
+        return User(self.client, resp["data"])
+
     def __init__(self, client, obj):
         super().__init__(client)
         self.unique += [
