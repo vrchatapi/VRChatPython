90987aaa6db248f6050d715e4840010d367b688e
Katistix <33645731+Katistic@users.noreply.github.com>
2020-06-08 08:58:57 +1000

    Update _hardtyping.py

--- vrcpy/_hardtyping.py
+++ vrcpy/_hardtyping.py
@@ -6,3 +6,9 @@ oBoolean = Optional[bool]
 oInteger = Optional[int]
 
 AvatarList = Union[List[objects.Avatar], List[aobjects.Avatar]]
+UserList = Union[List[objects.User], List[objects.LimitedUser], List[aobjects.User],\
+    List[aobjects.LimitedUser]]
+
+Avatar = Union[objects.Avatar, aobjects.Avatar]
+User = Union[objects.User, objects.LimitedUser, aobjects.User, aobjects.LimitedUser]
+CurrentUser = Union[objects.CurrentUser, aobjects.CurrentUser]
