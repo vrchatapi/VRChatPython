b7168a2d11fa058c1cf7124da074e5624bd9dc3c
Katistix <33645731+Katistic@users.noreply.github.com>
2020-08-13 16:04:01 +1000

    Create WebSocket Client.py

---
+++ examples/sync/WebSocket Client.py
@@ -0,0 +1,31 @@
+from vrcpy.wss import WSSClient
+import time
+
+class Client(WSSClient):
+    def on_friend_location(self, friend, world, location, instance):
+        print("Friend changed world.")
+
+    def on_friend_offline(self, friend):
+        print("Friend went offline.")
+
+    def on_friend_active(self, friend):
+        print("Friend is now active.")
+
+    def on_notification(self, notification):
+        print("Got a notification.")
+
+    def wait_loop(self):
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.logout()
+
+    def __init__(self):
+        super().__init__()
+        print("top: {}".format(hasattr(self, "loggedIn")))
+
+        self.login2fa(input("Username: "), input("Password: "), input("2FA Code: "), True)
+        self.wait_loop()
+
+c = Client()
