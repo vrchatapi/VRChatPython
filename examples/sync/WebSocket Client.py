641662a1e12ec4a28d7a601ce9db6d8d1210449e
Katistix <33645731+Katistic@users.noreply.github.com>
2020-08-15 15:52:35 +1000

    Update WebSocket Client.py

--- examples/sync/WebSocket Client.py
+++ examples/sync/WebSocket Client.py
@@ -3,18 +3,23 @@ import time
 
 class Client(WSSClient):
     def on_friend_location(self, friend, world, location, instance):
-        print("Friend changed world.")
+        print("{} is now in {}.".format(friend.displayName, world.name))
 
     def on_friend_offline(self, friend):
-        print("Friend went offline.")
+        print("{} went offline.".format(friend.displayName))
 
     def on_friend_active(self, friend):
-        print("Friend is now active.")
+        print("{} is now {}.".format(friend.displayName, friend.state))
+
+    def on_friend_online(self, friend):
+        print("{} is now online.".format(friend.displayName))
 
     def on_notification(self, notification):
-        print("Got a notification.")
+        print("Got a {} notification from {}.".format(notification.type, notification.senderUsername))
 
     def wait_loop(self):
+        self.login2fa(input("Username: "), input("Password: "), input("2FA Code: "), True)
+
         try:
             while True:
                 time.sleep(1)
@@ -23,8 +28,6 @@ class Client(WSSClient):
 
     def __init__(self):
         super().__init__()
-
-        self.login2fa(input("Username: "), input("Password: "), input("2FA Code: "), True)
         self.wait_loop()
 
 c = Client()
