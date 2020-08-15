7ff79c736f7b0fa523d2f824ef69360716c88ea3
Katistix <33645731+Katistic@users.noreply.github.com>
2020-08-15 15:57:02 +1000

    Async WebSocket Client example

---
+++ examples/async/WebSocket Client.py
@@ -0,0 +1,30 @@
+class AClient(AWSSClient):
+    async def on_friend_location(self, friend, world, location, instance):
+        print("{} is now in {}.".format(friend.displayName, world.name))
+
+    async def on_friend_offline(self, friend):
+        print("{} went offline.".format(friend.displayName))
+
+    async def on_friend_active(self, friend):
+        print("{} is now {}.".format(friend.displayName, friend.state))
+
+    async def on_friend_online(self, friend):
+        print("{} is now online.".format(friend.displayName))
+
+    async def on_notification(self, notification):
+        print("Got a {} notification from {}.".format(notification.type, notification.senderUsername))
+
+    async def wait_loop(self):
+        await self.login2fa(input("Username: "), input("Password: "), input("2FA Code: "), True)
+
+        while True:
+            await asyncio.sleep(1)
+
+    def __init__(self):
+        super().__init__()
+        try:
+            asyncio.get_event_loop().run_until_complete(self.wait_loop())
+        except KeyboardInterrupt:
+            asyncio.get_event_loop().run_until_complete(self.logout())
+
+c = AClient()
