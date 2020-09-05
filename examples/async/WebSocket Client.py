1562beb02b0aca824d5f079e2bf02efd189404be
Katistix <33645731+Katistic@users.noreply.github.com>
2020-09-05 19:06:24 +1000

    Decorator examples

--- examples/async/WebSocket Client.py
+++ examples/async/WebSocket Client.py
@@ -1,11 +1,22 @@
 from vrcpy.wss import AWSSClient
 import asyncio
-import time
+
+"""
+2 examples
+
+1. Class inheratence
+2. Decorators
+
+Remember you don't have to overwrite every event, just the ones you want to use!
+"""
+
+# Example 1
+
 
 class AClient(AWSSClient):
     async def on_friend_location(self, friend, world, location, instance):
         print("{} is now in {}.".format(friend.displayName,
-            "a private world" if location == None else world.name))
+                                        "a private world" if location == None else world.name))
 
     async def on_friend_offline(self, friend):
         print("{} went offline.".format(friend.displayName))
@@ -26,7 +37,8 @@ class AClient(AWSSClient):
         print("{} has updated their profile/account.".format(friend.displayName))
 
     async def on_notification(self, notification):
-        print("Got a {} notification from {}.".format(notification.type, notification.senderUsername))
+        print("Got a {} notification from {}.".format(
+            notification.type, notification.senderUsername))
 
     async def on_unhandled_event(self, event, content):
         print("Recieved unhandled event '{}'.".format(event))
@@ -50,4 +62,78 @@ class AClient(AWSSClient):
         except KeyboardInterrupt:
             asyncio.get_event_loop().run_until_complete(self.logout())
 
+
 c = AClient()
+
+# Example 2
+
+client = AWSSClient()
+
+
+@client.event
+async def on_friend_location(friend, world, location, instance):
+    print("{} is now in {}.".format(friend.displayName,
+                                    "a private world" if location == None else world.name))
+
+
+@client.event
+async def on_friend_offline(friend):
+    print("{} went offline.".format(friend.displayName))
+
+
+@client.event
+async def on_friend_active(friend):
+    print("{} is now {}.".format(friend.displayName, friend.state))
+
+
+@client.event
+async def on_friend_online(friend):
+    print("{} is now online.".format(friend.displayName))
+
+
+@client.event
+async def on_friend_add(friend):
+    print("{} is now your friend.".format(friend.displayName))
+
+
+@client.event
+async def on_friend_delete(friend):
+    print("{} is no longer your friend.".format(friend.displayName))
+
+
+@client.event
+async def on_friend_update(friend):
+    print("{} has updated their profile/account.".format(friend.displayName))
+
+
+@client.event
+async def on_notification(notification):
+    print("Got a {} notification from {}.".format(
+        notification.type, notification.senderUsername))
+
+
+@client.event
+async def on_unhandled_event(event, content):
+    print("Recieved unhandled event '{}'.".format(event))
+
+
+@client.event
+async def on_connect():
+    print("Connected to wss pipeline.")
+
+
+@client.event
+async def on_disconnect():
+    print("Disconnected from wss pipeline.")
+
+
+async def wait_loop():
+    await client.login2fa(input("Username: "), input("Password: "), input("2FA Code: "), True)
+
+    while True:
+        await asyncio.sleep(1)
+
+try:
+    asyncio.get_event_loop().run_until_complete(wait_loop())
+except KeyboardInterrupt:
+    asyncio.get_event_loop().run_until_complete(client.logout())
--- examples/sync/WebSocket Client.py
+++ examples/sync/WebSocket Client.py
@@ -1,10 +1,22 @@
 from vrcpy.wss import WSSClient
 import time
 
+"""
+2 examples
+
+1. Class inheratence
+2. Decorators
+
+Remember you don't have to overwrite every event, just the ones you want to use!
+"""
+
+# Example 1
+
+
 class Client(WSSClient):
     def on_friend_location(self, friend, world, location, instance):
         print("{} is now in {}.".format(friend.displayName,
-            "a private world" if location == None else world.name))
+                                        "a private world" if location == None else world.name))
 
     def on_friend_offline(self, friend):
         print("{} went offline.".format(friend.displayName))
@@ -25,7 +37,8 @@ class Client(WSSClient):
         print("{} has updated their profile/account.".format(friend.displayName))
 
     def on_notification(self, notification):
-        print("Got a {} notification from {}.".format(notification.type, notification.senderUsername))
+        print("Got a {} notification from {}.".format(
+            notification.type, notification.senderUsername))
 
     def on_unhandled_event(self, event, content):
         print("Recieved unhandled event '{}'.".format(event))
@@ -49,4 +62,75 @@ class Client(WSSClient):
         super().__init__()
         self.wait_loop()
 
+
 c = Client()
+
+# Example 2
+
+client = WSSClient()
+
+
+@client.event
+def on_friend_location(friend, world, location, instance):
+    print("{} is now in {}.".format(friend.displayName,
+                                    "a private world" if location == None else world.name))
+
+
+@client.event
+def on_friend_offline(friend):
+    print("{} went offline.".format(friend.displayName))
+
+
+@client.event
+def on_friend_active(friend):
+    print("{} is now {}.".format(friend.displayName, friend.state))
+
+
+@client.event
+def on_friend_online(friend):
+    print("{} is now online.".format(friend.displayName))
+
+
+@client.event
+def on_friend_add(friend):
+    print("{} is now your friend.".format(friend.displayName))
+
+
+@client.event
+def on_friend_delete(friend):
+    print("{} is no longer your friend.".format(friend.displayName))
+
+
+@client.event
+def on_friend_update(friend):
+    print("{} has updated their profile/account.".format(friend.displayName))
+
+
+@client.event
+def on_notification(notification):
+    print("Got a {} notification from {}.".format(
+        notification.type, notification.senderUsername))
+
+
+@client.event
+def on_unhandled_event(event, content):
+    print("Recieved unhandled event '{}'.".format(event))
+
+
+@client.event
+def on_connect():
+    print("Connected to wss pipeline.")
+
+
+@client.event
+def on_disconnect():
+    print("Disconnected from wss pipeline.")
+
+
+client.login2fa(input("Username: "), input("Password: "), input("2FA Code: "), True)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    client.logout()
