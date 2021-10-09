c9f92a244b435000b9017c96ae902fd5c2df0c2b
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-09 12:29:33 +1100

    Create config.py

---
+++ vrcpy/types/config.py
@@ -0,0 +1,8 @@
+﻿class Announcement:
+    __slots__ = ("name", "text")
+
+class DynamicWorldRow:
+    __slots__ = (
+        "index", "name", "platform", "sort_heading",
+        "sort_order", "sort_ownership"
+    )
