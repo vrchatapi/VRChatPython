8834341e494361962d6f11a7d569aa71b79fce31
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-09 12:01:56 +1100

    Create user.py

---
+++ vrcpy/user.py
@@ -0,0 +1,11 @@
+﻿from .model import Model
+
+class User(Model):
+    __slots__ = (
+        "allow_avatar_copying", "bio", "bio_links",
+        "current_avatar_image_url", "current_avatar_thumbnail_image_url",
+        "date_joined", "developer_type", "display_name", "friend_key", "id",
+        "instance_id", "is_friend", "last_login", "last_platform", "location",
+        "location", "profile_pic_override", "state", "status",
+        "status_description", "tags", "user_icon", "username", "world_id"
+    )
