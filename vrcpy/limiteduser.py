b78cfbece2e57dcd7d80da3fb55d6ed76c202a8d
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 02:05:49 +1100

    Create limiteduser.py

---
+++ vrcpy/limiteduser.py
@@ -0,0 +1,10 @@
+﻿from .model import Model
+
+class LimitedUser(Model):
+    __slots__ = (
+        "bio", "current_avatar_image_url",
+        "current_avatar_thumbnail_image_url", "developer_type",
+        "display_name", "fallback_avatar", "id", "is_friend", "last_platform",
+        "profile_pic_override", "status", "status_description", "tags",
+        "user_icon", "username"
+    )
