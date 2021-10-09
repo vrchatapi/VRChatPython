edec2fdc6ec2007c69a01e83810e37bb109dd03d
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 07:32:38 +1100

    Update model.py

--- vrcpy/model.py
+++ vrcpy/model.py
@@ -1,4 +1,19 @@
-﻿class Model:
+﻿import time
+
+__basetypes__ = (
+    str, int, list, dict
+)
+
+class TypeCasts:
+    __casts__ = {
+        time.struct_time: TypeCasts.struct_time
+    }
+
+    @staticmethod
+    def struct_time(self, attr: str) -> time.struct_time:
+        return time.strptime(attr, "%Y-%m-%dT%H:%M:%S%z")
+
+class Model:
     __slots__ = ("client", "loop")
 
     def __init__(self, client, loop, data):
@@ -6,7 +21,9 @@
         self.client = client
 
         for attr in data:
-            setattr(self, self._fix_attr_name(attr), data[attr])
+            fixed_attr = self._fix_attr_name(attr)
+            setattr(self, fixed_attr, data[attr])
+            self._fix_attr_type(fixed_attr)
 
     def _fix_attr_name(self, name):
         name_fin = ""
@@ -18,5 +35,14 @@
                     name_fin += f"_{char.lower()}"
             else:
                 name_fin += char
-
         return name_fin
+
+    def _fix_attr_type(self, attr):
+        if hasattr(self, "__types__") and attr in self.__types__:
+            type_of = self.__types__[attr]
+
+            if type_of in __basetypes__:
+                setattr(self, attr, type_of(getattr(self, attr)))
+            elif type_of in TypeCasts.__casts__:
+                setattr(self, attr, TypeCasts.__casts__[type_of](
+                    getattr(self, attr)))
