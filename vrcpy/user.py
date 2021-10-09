09b3d7d9ce25413bf86c37cdb03f34b34edf8348
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 02:05:54 +1100

    Update user.py

--- vrcpy/user.py
+++ vrcpy/user.py
@@ -1,11 +1,8 @@
-﻿from .model import Model
+﻿from .limiteduser import LimitedUser
 
-class User(Model):
+class User(LimitedUser):
     __slots__ = (
-        "allow_avatar_copying", "bio", "bio_links",
-        "current_avatar_image_url", "current_avatar_thumbnail_image_url",
-        "date_joined", "developer_type", "display_name", "friend_key", "id",
-        "instance_id", "is_friend", "last_login", "last_platform", "location",
-        "location", "profile_pic_override", "state", "status",
-        "status_description", "tags", "user_icon", "username", "world_id"
+        "allow_avatar_copying", "bio_links", "date_joined",
+        "friend_key", "instance_id", "last_login", "location",
+        "location", "state", "world_id"
     )
