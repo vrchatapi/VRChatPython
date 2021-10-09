d605814e539c2d02670863c7dddc1059ee064b13
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 04:25:15 +1100

    Create client.py

---
+++ vrcpy/client.py
@@ -0,0 +1,34 @@
+﻿from .errors import ClientErrors
+from .http import Request
+
+from .config import Config
+
+class Client:
+    def __init__(self, loop):
+        self.loop = loop
+        self.me = None
+        self.config = None
+        self.request = Request()
+
+        self._logged_in = False
+
+    def _auth_required(self, func):
+        async def _func(*args, **kwargs):
+            if not self._logged_in:
+                raise ClientErrors.NotLoggedIn(
+                    "Client is not logged in!",
+                    func
+                )
+            await func(*args, **kwargs)
+
+        setattr(self, func.__name__, _func)
+
+    async def fetch_config(self):
+        async with self.request.session.get(
+            self.request.base + "/config") as resp:
+
+            assert resp.status == 200
+            json = await resp.json()
+            self.config = Config(self.loop, json)
+
+        return self.config
