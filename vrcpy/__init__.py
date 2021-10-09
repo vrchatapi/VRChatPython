a77e8bb4a762a07cc6ccabb9be46ae31896e4dca
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-09 11:34:21 +1100

    Fresh start

--- vrcpy/__init__.py
+++ vrcpy/__init__.py
@@ -1,15 +1,3 @@
-from vrcpy.user import *
-from vrcpy.file import *
-from vrcpy.world import *
-from vrcpy.client import *
-from vrcpy.avatar import *
-from vrcpy.favorite import *
-from vrcpy.moderation import *
-from vrcpy.notification import *
-
-import vrcpy.util
-import vrcpy.errors
-
 __title__ = "vrcpy"
 __author__ = "Katistic"
 __version__ = "0.8.5"
