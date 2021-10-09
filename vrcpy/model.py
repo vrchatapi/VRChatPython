2e821caf2790b2558fc019b5ea47ddbcdb1daeba
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 01:48:33 +1100

    Update model.py

--- vrcpy/model.py
+++ vrcpy/model.py
@@ -1,5 +1,21 @@
 ﻿class Model:
     __slots__ = ("loop")
 
-    def __init__(self, loop):
+    def __init__(self, loop, data):
         self.loop = loop
+
+        for attr in data:
+            setattr(self, self._fix_attr_name(attr), data[attr])
+
+    def _fix_attr_name(self, name):
+        name_fin = ""
+        for char in name:
+            if char == char.upper():
+                if char == name[0]:
+                    name_fin += char.lower()
+                else:
+                    name_fin += f"_{char.lower()}"
+            else:
+                name_fin += char
+
+        return name_fin
