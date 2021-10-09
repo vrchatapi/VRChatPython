55717cfdf52cdd7451d54e3a974b6dc69cbc036d
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 06:34:59 +1100

    Update config.py

--- vrcpy/types/config.py
+++ vrcpy/types/config.py
@@ -1,8 +1,22 @@
 ﻿class Announcement:
     __slots__ = ("name", "text")
 
+    __types__ = {
+        "name": str,
+        "text": str
+    }
+
 class DynamicWorldRow:
     __slots__ = (
         "index", "name", "platform", "sort_heading",
         "sort_order", "sort_ownership"
     )
+
+    __types__ = {
+        "index": int,
+        "name": str,
+        "platform": str,
+        "sort_heading": str,
+        "sort_order": str,
+        "sort_ownership": str
+    }
