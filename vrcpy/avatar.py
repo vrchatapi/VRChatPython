6e1dafdd42f1fa31891d6a3fd2bcc4992a8e4e9d
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-09 11:34:53 +1100

    Update avatar.py

---
+++ vrcpy/avatar.py
@@ -0,0 +1,9 @@
+﻿from .model import Model
+
+class Avatar(Model):
+    __slots__ = (
+        "asset_url", "asset_url_object", "author_id", "author_name",
+        "created_at", "description", "featured", "id", "image_url", "name",
+        "release_status", "tags", "thumbnail_image_url", "unity_package_url",
+        "unity_packages", "updated_at", "version"
+    )
