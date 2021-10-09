90b15ed4beb1ea61b3117d7c62a7244ddc617800
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 07:25:02 +1100

    Create threadwrap.py

---
+++ vrcpy/util/threadwrap.py
@@ -0,0 +1,15 @@
+﻿import threading
+
+class ThreadWrap(threading.Thread):
+    def __init__(self, *args, start=True, **kwargs):
+        kwargs["target"] = self._method
+        super().__init__(*args, **kwargs)
+
+        self.method = None
+        self.daemon = True
+
+    def _method(self):
+        self.method()
+
+    def set_method(self, method, *args, **kwargs):
+        self.method = lambda: method(*args, **kwargs)
