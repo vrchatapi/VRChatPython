0a576cb9d0fa95aaf22202c4ddbe4fc506743bf0
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-09 11:55:33 +1100

    Create file.py

---
+++ vrcpy/types/file.py
@@ -0,0 +1,16 @@
+﻿class FileVersion:
+    __slots__ = (
+        "version", "status", "created_at", "file", "delta", "signature"
+    )
+
+class FileAsset:
+    __slots__ = (
+        "file_name", "url", "md5", "size_in_bytes",
+        "status", "category", "upload_id"
+    )
+
+class FileStatus:
+    __slots__ = (
+        "upload_id", "file_name", "next_part_number",
+        "max_parts", "parts", "etags"
+    )
