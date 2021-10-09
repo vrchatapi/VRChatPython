2f21f1464168e3dfd46e4814b4840b274a8f6691
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 02:17:54 +1100

    Create limitedworld.py

---
+++ vrcpy/limitedworld.py
@@ -0,0 +1,10 @@
+﻿from .model import Model
+
+class LimitedWorld(Model):
+    __slots__ = (
+        "author_id", "author_name", "capacity", "created_at", "favorites",
+        "heat", "id", "image_url", "labs_publication_date", "name",
+        "occupants", "organization", "popularity", "publication_date",
+        "release_status", "tags", "thumbnail_image_url", "unity_packages",
+        "updated_at"
+    )
