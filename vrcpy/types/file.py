23cdc8280099e556696deca0fef39978c7e12101
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 06:33:08 +1100

    Update file.py

--- vrcpy/types/file.py
+++ vrcpy/types/file.py
@@ -1,7 +1,4 @@
-﻿class FileVersion:
-    __slots__ = (
-        "version", "status", "created_at", "file", "delta", "signature"
-    )
+﻿import time
 
 class FileAsset:
     __slots__ = (
@@ -9,8 +6,39 @@ class FileAsset:
         "status", "category", "upload_id"
     )
 
+    __types__ = {
+        "file_name": str,
+        "url": str,
+        "md5": str,
+        "size_in_bytes": int,
+        "status": str,
+    }
+
+class FileVersion:
+    __slots__ = (
+        "version", "status", "created_at", "file", "delta", "signature"
+    )
+
+    __types__ = {
+        "version": int,
+        "status": str,
+        "created_at": time.struct_time,
+        "file": FileAsset,
+        "delta": FileAsset,
+        "signature": FileAsset
+    }
+
 class FileStatus:
     __slots__ = (
         "upload_id", "file_name", "next_part_number",
         "max_parts", "parts", "etags"
     )
+
+    __types__ = {
+        "upload_id": str,
+        "file_name": str,
+        "next_part_number": int,
+        "max_parts": int,
+        "parts": list,
+        "etags": list
+    }
