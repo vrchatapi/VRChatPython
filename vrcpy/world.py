6b82968bae213b8c7b1915dd83afbf6240b0e94f
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 02:18:02 +1100

    Create world.py

---
+++ vrcpy/world.py
@@ -0,0 +1,10 @@
+﻿from .limitedworld import LimitedWorld
+
+class World(LimitedWorld):
+    __slots__ = (
+        "asset_url", "asset_url_obj", "description", "featured"
+        "instance", "labs_publication_date", "name", "namespace",
+        "plugin_url_object", "preview_youtube_id", "private_occupants",
+        "public_occupants", "unity_package_url_object",
+        "version", "visits"
+    )
