f0b1786c762ec707c09f90629398b7b2cbff4ac4
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 01:59:44 +1100

    Create favoritegroup.py

---
+++ vrcpy/favoritegroup.py
@@ -0,0 +1,24 @@
+﻿from .model import Model
+
+class FavoriteGroup(Model):
+    @staticmethod
+    def favorite_group(self, loop, data):
+        groups = {
+            "world": FavoriteWorldGroup,
+            "friend": FavoriteFriendGroup,
+            "avatar": FavoriteAvatarGroup
+        }
+
+        return groups[data["type"]](loop, data)
+
+    def __init__(self, loop, data):
+        super().__init__(loop, data)
+
+class FavoriteWorldGroup(FavoriteGroup):
+    pass
+
+class FavoriteFriendGroup(FavoriteGroup):
+    pass
+
+class FavoriteAvatarGroup(FavoriteGroup):
+    pass
