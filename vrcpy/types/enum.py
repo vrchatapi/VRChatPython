4f0aa695642040d0b22a380c502da9b601e7d67e
Katistix <33645731+Katistic@users.noreply.github.com>
2021-10-10 10:44:35 +1100

    Create enum.py

---
+++ vrcpy/types/enum.py
@@ -0,0 +1,16 @@
+﻿from enum import Enum
+
+class UserStatus(Enum):
+    ACTIVE = "active"
+    JOIN_ME = "join me"
+    ASK_ME = "ask me"
+    BUSY = "busy"
+    OFFLINE = "offline"
+
+class PlayerModerationType(Enum):
+    MUTE = "mute"
+    UNMUTE = "unmute"
+    BLOCK = "block"
+    UNBLOCK = "unblock"
+    HIDEAVATAR = "hideAvatar"
+    SHOWAVATAR = "showAvatar"
